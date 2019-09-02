dfeb645d5875a40f2ee04f2001350f0f3cd6741d
lvyufeng <lvyufeng2007@hotmail.com>
2019-09-02 23:25:15 +0800

    part 3

--- tests/test_tensor_mul.py
+++ tests/test_tensor_mul.py
@@ -1,18 +1,18 @@
 import unittest
 
-from autograd.tensor import Tensor,add
-class TestTensorAdd(unittest.TestCase):
-    def test_simple_add(self):
+from autograd.tensor import Tensor,mul
+class TestTensorMul(unittest.TestCase):
+    def test_simple_mul(self):
         t1 = Tensor([1,2,3],requires_grad=True)
         t2 = Tensor([4,5,6],requires_grad=True)
 
-        t3 = add(t1,t2)
+        t3 = mul(t1,t2)
         t3.backward(Tensor([-1,-2,-3]))
 
-        assert t1.grad.data.tolist() == [-1,-2,-3]
-        assert t2.grad.data.tolist() == [-1,-2,-3]
+        assert t1.grad.data.tolist() == [-4, -10, -18]
+        assert t2.grad.data.tolist() == [-1, -4 , -9 ]
 
-    def test_broadcast_add(self):
+    def test_broadcast_mul(self):
         # broadcasting? A couple of things:
         # IF I do t1 + t2 and t1.shape == t2.shape, it's obvious what to do
         # but I'm alse allowed to add 1s to the beginning of either shape.
@@ -26,18 +26,18 @@ class TestTensorAdd(unittest.TestCase):
         t1 = Tensor([[1,2,3],[4,5,6]],requires_grad=True)
         t2 = Tensor([7,8,9],requires_grad=True)
 
-        t3 = add(t1,t2)
+        t3 = mul(t1,t2)
         t3.backward(Tensor([[1,1,1],[1,1,1]]))
 
-        assert t1.grad.data.tolist() == [[1,1,1],[1,1,1]]
-        assert t2.grad.data.tolist() == [2,2,2]
+        assert t1.grad.data.tolist() == [[7,8,9],[7,8,9]]
+        assert t2.grad.data.tolist() == [5, 7, 9]
 
-    def test_broadcast_add2(self):
+    def test_broadcast_mul2(self):
         t1 = Tensor([[1,2,3],[4,5,6]],requires_grad=True) # (2,3)
         t2 = Tensor([[7,8,9]],requires_grad=True)         # (1,3)
 
-        t3 = add(t1,t2)
+        t3 = mul(t1,t2)
         t3.backward(Tensor([[1,1,1],[1,1,1]]))
 
-        assert t1.grad.data.tolist() == [[1,1,1],[1,1,1]]
-        assert t2.grad.data.tolist() == [[2,2,2]]
+        assert t1.grad.data.tolist() == [[7,8,9],[7,8,9]]
+        assert t2.grad.data.tolist() == [[5, 7 ,9]]
