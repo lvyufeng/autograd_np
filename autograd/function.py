49363a616c568d9daa2f8ce18f6fc81bd4c8c231
Yufeng Lyu <lvyufeng@cqu.edu.cn>
2021-01-05 22:51:43 +0800

    add sigmoid activation function

--- autograd/function.py
+++ autograd/function.py
@@ -15,3 +15,15 @@ def tanh(tensor: Tensor) -> Tensor:
     else:
         depends_on = []
     return Tensor(data, requires_grad, depends_on)
+
+def sigmoid(tensor: Tensor) -> Tensor:
+    data = 1 / (1 + np.exp(-tensor.data))
+    requires_grad = tensor.requires_grad
+
+    if requires_grad:
+        def grad_fn(grad: np.ndarray) -> np.ndarray:
+            return grad * (data * (1 - data))
+        depends_on = [Dependency(tensor, grad_fn)]
+    else:
+        depends_on = []
+    return Tensor(data, requires_grad, depends_on)
